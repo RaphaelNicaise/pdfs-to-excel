fccf703e910ac1b3c6530d36049800021bcc9634
RaphaelNicaise <raphanicaise@gmail.com>
2025-04-24 00:41:41 -0300

    Chequeo formato del archivo para que solo se procesen los que deben

--- processing.py
+++ processing.py
@@ -4,10 +4,32 @@ import os
 import pandas as pd
 from pdfquery import PDFQuery
 
-from scrap_pdf import get_datos_from_pdf
+from scrap_pdf import get_datos_from_pdf, check_format
 from utils import listar_archivos_pdf
 
-def trasnform_df(df):
+def trasnform_df(df)->pd.DataFrame:
+    """ Transformar el df para que tenga el formato correcto
+    y los tipos de datos correctos
+
+    Args:
+        df (pd.Dataframe): dataframe a transformar
+
+    Returns:
+        pd.DataFrame: dataframe transformado
+    """
+    
+    df['documentos_anexos'] = df['documentos_anexos'].str.extract(r'Destinacion:\s*(.*?)\s*F\. Ofic')
+    
+    df['conductor'] = df['conductor'].str.extract(r'CONDUCTOR\s*\d*:\s*(.*)')
+    
+    
+    df = df.astype({
+        'valor_FOT': 'float',
+        'flete_usd': 'float',
+        'cantidad_bultos': 'int',
+        'peso_bruto': 'float'      
+    })
+    
     return df
 
 
@@ -23,6 +45,11 @@ def process_pdf(archivo:str)->dict:
     try:
         pdf = PDFQuery(archivo)
         pdf.load(0)
+        
+        if not check_format(pdf):
+            print(f"Formato incorrecto para {archivo}")
+            return None
+            
         data = get_datos_from_pdf(pdf)
         print(f"Procesado {archivo} en {time.time() - start:.2f} segundos")
         return data
@@ -43,7 +70,7 @@ def process_all_pdfs(archivos: list[str])->list:
     start = time.time()
     
     try:
-        data = list(map(process_pdf, archivos))
+        data = [resultado for resultado in map(process_pdf, archivos) if resultado is not None]
     except Exception as e:
         print(f"Error procesando archivos: {e}")
         return None
@@ -59,7 +86,10 @@ def main_process(archivos: list, destino: str) -> None:
     """
     lista = process_all_pdfs(archivos)
     
-    nombres_archivos = [os.path.splitext(os.path.basename(archivo))[0] for archivo in archivos]
+    nombres_archivos = [
+        os.path.splitext(os.path.basename(archivo))[0]
+        for archivo, resultado in zip(archivos, lista) if resultado is not None
+    ]
     
     df = pd.DataFrame(lista)
     
