a07bbbfd1b9ca7348283ee1b856929d64b51d3a5
RaphaelNicaise <raphanicaise@gmail.com>
2025-05-01 14:37:13 -0300

    Fix

--- ProcesamientoAG/detalle_operaciones.py
+++ ProcesamientoAG/detalle_operaciones.py
@@ -8,13 +8,14 @@ from openpyxl.styles import Alignment
 import xlwings
 from xlwings import Book
 
+
 def get_asset_path(relative_path):
     """Obtiene la ruta correcta del archivo, ya sea en desarrollo o en el ejecutable."""
     try:
-        #  cuando es ejecutable empaquetado
+        # Cuando se ejecuta como ejecutable empaquetado
         base_path = sys._MEIPASS
     except AttributeError:
-        # cuand script en desarrollo
+        # Cuando se ejecuta como script en desarrollo
         base_path = os.path.abspath(".")
     return os.path.join(base_path, relative_path)
 
@@ -31,50 +32,40 @@ def main_process_detalle_operacion(ruta_excel_base, ruta_destino, excel_datos, a
     """
     ruta_excel_base = get_asset_path(ruta_excel_base)
 
+    # Crear nombre del nuevo archivo
     nombre_empresa = os.path.basename(excel_datos).split('AG-')[1].split('.xlsx')[0]
-    nombre_archivo = "Detalle Operaciones " + nombre_empresa + ".xlsx"
+    nombre_archivo = "Detalle Proceso " + nombre_empresa + ".xlsx"
     ruta_final = os.path.join(ruta_destino, nombre_archivo)
 
+    # Copiar plantilla a destino
     shutil.copy(ruta_excel_base, ruta_final)
 
+    # Abrir archivo copiado
     wb = load_workbook(ruta_final)
     wb.template = False
     ws = wb.active
-<<<<<<< HEAD
 
-=======
     # Insertar mes y año
->>>>>>> 377b587b9b393fe0478c2b3f08e80c853bfb72cf
     ws['B8'] = mes
     ws['B8'].alignment = Alignment(horizontal='left', vertical='center')
 
     ws['E8'] = anio
     ws['E8'].alignment = Alignment(horizontal='left', vertical='center')
 
-<<<<<<< HEAD
-    
-=======
     # Nombre de la firma (harcodeado por ahora)
->>>>>>> 377b587b9b393fe0478c2b3f08e80c853bfb72cf
+    
     ws['B10'] = nombre_empresa
     ws['B10'].alignment = Alignment(horizontal='left', vertical='center')
-    
+
+    # Leer datos desde el archivo Excel de datos
     df = pd.read_excel(excel_datos, engine='openpyxl')
+
     df_final = df.loc[:, ['FECHA CARGA', 'MIC - DTA', 'D.D.T', 'ORDEN', 'FACTURA Nº']]
     df_final['FECHA CARGA'] = pd.to_datetime(df_final['FECHA CARGA'], dayfirst=True, errors='coerce')
     # Ordenar por la columna 'FECHA CARGA' de menor a mayor
     df_final = df_final.sort_values(by='FECHA CARGA', ascending=True).reset_index(drop=True)
     df_final['FECHA CARGA'] = df_final['FECHA CARGA'].dt.strftime('%d/%m/%Y')
-
-<<<<<<< HEAD
     
-    total_unidades = len(df)
-    ws['B11'] = total_unidades
-    ws['B11'].alignment = Alignment(horizontal='left', vertical='center')
-
-    
-    for i, row in df.iterrows():
-=======
     # Insertar total de unidades
     total_unidades = len(df_final)
     ws['B11'] = total_unidades
@@ -82,12 +73,12 @@ def main_process_detalle_operacion(ruta_excel_base, ruta_destino, excel_datos, a
 
     # Insertar datos desde fila 14 (índice 13)
     for i, row in df_final.iterrows():
->>>>>>> 377b587b9b393fe0478c2b3f08e80c853bfb72cf
         for j, campo in enumerate(['FECHA CARGA', 'MIC - DTA', 'D.D.T', 'ORDEN', 'FACTURA Nº']):
             ws.cell(row=14 + i, column=1 + j, value=row[campo])
 
     # Guardar
     wb.save(ruta_final)
+    print(f"Archivo guardado en: {ruta_final}")
     
     wb = Book(ruta_final)
     ws =  wb.sheets[0]
